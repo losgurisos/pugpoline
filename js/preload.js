280392b40cdbedc18069275567f2e56c9b58afbf
Andres Peña Luna <Andres Pea Luna>
2016-02-14 23:58:39 -0300

    tab identation

--- js/preload.js
+++ js/preload.js
@@ -3,7 +3,7 @@
 
 function preload() {
 
-    game.load.spritesheet('pug', 'assets/pug.png', 32, 48);
-    game.load.image('trampoline', 'assets/trampoline.png', 800, 64, 15);
+	game.load.spritesheet('pug', 'assets/pug.png', 32, 48);
+	game.load.image('trampoline', 'assets/trampoline.png', 800, 64, 15);
 
 }
